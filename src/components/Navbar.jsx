0ce4dcb844c4f9423f096c1daafbbffaf52fbdd0
abraham <abrahamkifle1212@gmail.com>
2026-01-16 17:06:56 +0300

    feat: set up initial React application with routing and a global navigation layout.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className='navbar'>
         <img src={logo} alt="" width="130px" />
         <ul>
-            <li>Home</li>
-            <li>Product</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li><Link to={"/"}>Home</Link></li>
+            <li><Link to={"/product"}>Product</Link></li>
+            <li><Link to={"/about"}>About</Link></li>
+            <li><Link to={"/contact"}>Contact</Link></li>
         </ul>
         <button>Get Started</button>
     </div>
